7c061cf4d1dd52dd166a74c756c5c7dadb189767
jmokate <jmokate@gmail.com>
2019-11-13 15:44:18 -0600

    working on local storage

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const submitItem = document.querySelector("#submitBtn"); // submit button
 const ulEl = document.getElementById("todo-list"); // the ul for the todolist
 const clearSelect = document.querySelector("#clearSelected");
 const clearAll = document.querySelector("#clearAll");
+const inputArray = [];
+const liAdd = document.createElement("li");
+//event listener for addItem function
 formEl.addEventListener("submit", addItem);
-
 function addItem(e) {
   e.preventDefault();
   console.log("hello");
@@ -19,8 +21,6 @@ function addItem(e) {
   checkBox.setAttribute("type", "checkbox"); // checkbox attributes
 
   //add checkbox chunk
-  // const checkBox = document.createElement("input");
-  // checkBox.setAttribute("type", "checkbox");
   liAdd.appendChild(checkBox);
   checkBox.addEventListener("click", function() {
     if (checkBox.checked == true) {
@@ -30,18 +30,19 @@ function addItem(e) {
     liAdd.style.textDecoration = "none";
   });
 
+  formEl.reset(); // resets input text field
+
   //adding list item chink
-  // const userInput = document.querySelector("#text-input").value; // gets user input
-  // const liAdd = document.createElement("li"); // create li
   liAdd.className = "list-item"; // add class to li
   liAdd.appendChild(document.createTextNode(userInput));
   ulEl.appendChild(liAdd);
 
-  formEl.reset(); // resets text-input field
+  //set local storage
+  inputArray.push(userInput);
+  localStorage.setItem("storageKey", JSON.stringify(inputArray));
+  console.log(inputArray);
 
   //add button chunk
-  // const deleteBtn = document.createElement("button");
-  // deleteBtn.textContent = "x";
   liAdd.appendChild(deleteBtn);
   deleteBtn.addEventListener("click", function() {
     liAdd.remove();
@@ -50,7 +51,6 @@ function addItem(e) {
   //clear selected chunk
   clearSelect.addEventListener("click", function() {
     if (checkBox.checked == true) {
-      //ulEl.removeChild(liAdd);
       liAdd.remove();
     }
   });
@@ -58,5 +58,34 @@ function addItem(e) {
   //clear all chunk
   clearAll.addEventListener("click", function() {
     liAdd.remove();
+    localStorage.clear();
   });
 }
+
+window.onload = function() {
+  example = JSON.parse(localStorage.getItem("storageKey"));
+  console.log(example);
+  ulEl.appendChild(liAdd);
+  liAdd.appendChild(document.createTextNode(example));
+
+  for (i = 0; i < example.length; i++) {
+    //console.log(example);
+    //console.log(localStorage.getItem(localStorage.key(i)));
+    //console.log(example.length);
+    //   liAdd.appendChild(document.createTextNode(example));
+    //   ulEl.appendChild(liAdd);
+  }
+};
+
+// if (example === null) {
+//   getStorage = [];
+//   return;
+// } else {
+//   getStorage = localStorage.getItem("tasks");
+//   liAdd.innerHTML = getStorage;
+// }
+
+//let getStorage = JSON.parse(localStorage.getItem("storageKey"));
+
+//localStorage.removeItem("");
+//array creation loop for local storage
