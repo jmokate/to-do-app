a1c0f61f1c67a06469fef327a254f9c8101d0995
jmokate <jmokate@gmail.com>
2019-11-16 09:44:45 -0600

    saving and deleting local starge working with single delete button

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ const submitItem = document.querySelector("#submitBtn"); // submit button
 const ulEl = document.getElementById("todo-list"); // the ul for the todolist
 const clearSelect = document.querySelector("#clearSelected");
 const clearAll = document.querySelector("#clearAll");
-const inputArray = [];
+const toDoArray = localStorage["storageKey"] //if "storageKey is true"
+  ? JSON.parse(localStorage["storageKey"]) //then parse storagekey
+  : []; // this will save previous userinputs and avoid overwriting/erasing previous everytime page is loaded
+
 const liAdd = document.createElement("li");
-//event listener for addItem function
-formEl.addEventListener("submit", addItem);
-function addItem(e) {
+const checkBox = document.createElement("input"); // create checkbox
+checkBox.setAttribute("type", "checkbox");
+const deleteBtn = document.createElement("button"); // create delete btn
+deleteBtn.textContent = "x";
+const userInput = document.querySelector("#text-input").value; // gets user input
+
+//event listener for new to-do function
+formEl.addEventListener("submit", createNewToDo);
+function createNewToDo(e) {
   e.preventDefault();
-  console.log("hello");
+  // console.log("hello");
   //function variables
   const userInput = document.querySelector("#text-input").value; // gets user input
   const liAdd = document.createElement("li"); // create li
@@ -30,19 +39,12 @@ function addItem(e) {
     liAdd.style.textDecoration = "none";
   });
 
-  formEl.reset(); // resets input text field
-
   //adding list item chink
   liAdd.className = "list-item"; // add class to li
   liAdd.appendChild(document.createTextNode(userInput));
   ulEl.appendChild(liAdd);
 
-  //set local storage
-  inputArray.push(userInput);
-  localStorage.setItem("storageKey", JSON.stringify(inputArray));
-  console.log(inputArray);
-
-  //add button chunk
+  //add delete button chunk
   liAdd.appendChild(deleteBtn);
   deleteBtn.addEventListener("click", function() {
     liAdd.remove();
@@ -60,32 +62,67 @@ function addItem(e) {
     liAdd.remove();
     localStorage.clear();
   });
+
+  formEl.reset(); // resets input text field
+
+  //set local storage
+  toDoArray.push(userInput);
+  saveToLocalStorage();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("storageKey", JSON.stringify(toDoArray));
 }
 
 window.onload = function() {
-  example = JSON.parse(localStorage.getItem("storageKey"));
-  console.log(example);
-  ulEl.appendChild(liAdd);
-  liAdd.appendChild(document.createTextNode(example));
+  const savedStorage = JSON.parse(localStorage.getItem("storageKey"));
 
-  for (i = 0; i < example.length; i++) {
-    //console.log(example);
-    //console.log(localStorage.getItem(localStorage.key(i)));
-    //console.log(example.length);
-    //   liAdd.appendChild(document.createTextNode(example));
-    //   ulEl.appendChild(liAdd);
-  }
+  savedStorage.forEach(function(item) {
+    let storageArray = savedStorage;
+
+    let storageLiEl = document.createElement("li");
+    let storageDeleteBtn = document.createElement("button");
+    storageDeleteBtn.textContent = "x";
+    let storageCheckBox = document.createElement("input"); // create checkbox
+    storageCheckBox.setAttribute("type", "checkbox");
+
+    ulEl.appendChild(storageLiEl);
+
+    storageLiEl.appendChild(document.createTextNode(item));
+    storageLiEl.appendChild(storageCheckBox);
+    storageLiEl.appendChild(storageDeleteBtn);
+
+    storageDeleteBtn.addEventListener("click", function() {
+      storageLiEl.remove();
+
+      removeArrayItem(item);
+
+      function removeArrayItem(deletedItem) {
+        // debugger;
+        //pass in to-do item text in deleted parameter
+        //use indexOf array method to return items index check
+        //store item index in a variable
+        //then splice
+        let index = storageArray.indexOf(deletedItem);
+        storageArray.splice(index, 1);
+        localStorage.setItem("storageKey", JSON.stringify(storageArray));
+        console.log(deletedItem);
+        console.log(index);
+        console.log(storageArray);
+      }
+    });
+  });
 };
 
-// if (example === null) {
-//   getStorage = [];
-//   return;
-// } else {
-//   getStorage = localStorage.getItem("tasks");
-//   liAdd.innerHTML = getStorage;
+// function deleteStorageItem() {
+//   let storageArray = savedStorage;
+
+//   storageArray.forEach(item => {
+//     let arrayItem = item;
+//     if (arrayItem == item) storageArray.splice(item, 1);
+//     storageArray.splice(item);
+
+//     console.log(storageArray);
+//     console.log(arrayItem);
+//   });
 // }
-
-//let getStorage = JSON.parse(localStorage.getItem("storageKey"));
-
-//localStorage.removeItem("");
-//array creation loop for local storage
